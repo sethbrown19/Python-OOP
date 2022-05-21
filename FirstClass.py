0905a8d3dd98134e92892ed52a6b7b0ff6d645db
Seth Brown <sethbrown19@outlook.com>
2022-05-21 14:41:03 -0400

    working on class understanding class structure in python

--- FirstClass.py
+++ FirstClass.py
@@ -1,4 +1,20 @@
 # Our first class
 
 class DodgeRam:
-    pass
+    
+    
+    def __init__(self, color, doors, wheels, model):
+        self.color = color
+        self.doors = doors
+        self.wheels = wheels
+        self.model = model
+    
+    def drive(self):
+        return f"Driving a dodge ram and it is the {self} instance\n"
+     
+
+d1 = DodgeRam()
+
+print(d1.drive())
+
+
